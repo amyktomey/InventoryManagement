28943a929447bf70ea0ec61483e25d27b47e4ef1
Amy Tomey <amy.tomey59@gmail.com>
2023-11-28 08:10:27 -0500

    project loads, getting exception error

--- Data/ProductList.cs
+++ Data/ProductList.cs
@@ -1,18 +1,14 @@
-﻿namespace InventoryBrowser.Maui.Data;
+﻿using System.Net.Http.Json;
+
+namespace InventoryBrowser.Maui.Data;
 
 public class ProductList : IProductList
 {
-    public async Task<IEnumerable<Product>> LoadProductsAsync()
+    private readonly HttpClient _httpClient = new ();
+    public async Task<IEnumerable<Product>?> LoadProductsAsync()
     {
-        var products = new[]
-        {
-            new Product("Lover's Bane", "Potion", "Blood red & black potion with a gold spark for creating passion", 12, 4),
-            new Product("Qilin", "Feat Charm", "Homebrew: +2 to AC, if dowmed HP drops to 1, not 0.", 10, 2),
-            new Product("Hair of the Dog", "D&D Dice Set", "Dice set made with a little magic and werewolf hair", 18, 1)
-        };
-
-        await Task.Delay(50);
-
+        var products = await _httpClient.GetFromJsonAsync<IEnumerable<Product>>(
+            "InventoryList.json");
         return products;
     }
 }
