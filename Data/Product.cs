bfd8747f7d1069308943befc8a72cb3445440ecd
Amy Tomey <amy.tomey59@gmail.com>
2023-11-30 12:07:23 -0500

    added database, appsettings.json, context files

--- Data/Product.cs
+++ Data/Product.cs
@@ -1,3 +1,12 @@
 ﻿namespace InventoryBrowser.Maui.Data;
 
-public record Product(string Name, string Type, string Description, decimal Price, int Quantity);
+//public record Product(string Name, string Type, string Description, decimal Price, int Quantity);
+public class Product
+{
+    public int Id { get; set; }
+    public string?Name { get; set; }
+    public string? Type { get; set; }
+    public string?Description{ get; set; }
+    public decimal? Price{ get; set; }
+    public int? Quantity { get; set; }
+}
---
+++ Data/ProductContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace InventoryBrowser.Maui.Data;
+    public class ProductContext : DbContext
+    {
+    protected readonly IConfiguration Configuration;
+    public ProductContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(Configuration.GetConnectionString("ProductInventory"));
+    }
+    public DbSet<Product> Inventory { get; set; }
+}
